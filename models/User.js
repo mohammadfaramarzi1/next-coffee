2f8f6a61f9979d6eff2696b36625713e45e532a7
MohammadFeri <faramarzi.dev@gmail.com>
2025-09-15 12:24:33 +0330

    feat: user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    role: {
+      type: String,
+      default: "USER",
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const model = mongoose.models.User || mongoose.model("User", schema);
+
+export default model;
