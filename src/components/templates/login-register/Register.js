e8a0e30fa08cd43fb7e30836eefc2a82825ce186
MohammadFeri <faramarzi.dev@gmail.com>
2025-09-16 09:52:53 +0330

    feat: signup

--- src/components/templates/login-register/Register.js
+++ src/components/templates/login-register/Register.js
@@ -1,34 +1,111 @@
 import { useState } from "react";
 import styles from "./register.module.css";
 import Sms from "./Sms";
+import { showSwal } from "@/utils/helpers";
+import { validateEmail, validatePassword, validatePhone } from "@/utils/auth";
 
 const Register = ({ showloginForm }) => {
   const [isRegisterWithPassword, setIsRegisterWithPassword] = useState(false);
   const [isRegisterWithOtp, setIsRegisterWithOtp] = useState(false);
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+
   const hideOtpForm = () => setIsRegisterWithOtp(false);
+
+  const signUp = async () => {
+    if (!name.trim()) {
+      return showSwal("نام را وارد بکنید", "error", "تلاش بیشتر");
+    }
+
+    const isValidPhone = validatePhone(phone);
+
+    if (!isValidPhone) {
+      return showSwal(
+        "شماره تماس وارد شده معتبر نمی باشد",
+        "error",
+        "تلاش بیشتر"
+      );
+    }
+
+    if (email) {
+      const isValidEmail = validateEmail(email);
+      if (!isValidEmail) {
+        return showSwal(
+          " ایمیل وارد شده معتبر نمی باشد",
+          "error",
+          "تلاش بیشتر"
+        );
+      }
+    }
+
+    const isValidPassword = validatePassword(password);
+
+    if (!isValidPassword) {
+      return showSwal(
+        " رمز عبور وارد شده معتبر نمی باشد",
+        "error",
+        "تلاش بیشتر"
+      );
+    }
+
+    const user = {
+      phone,
+      email,
+      name,
+      password,
+    };
+
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (res.status === 201) {
+      showSwal("ثبت نام با موفقیت انجام شد", "success", "ورود به پنل کاربری");
+    } else if (res.status === 422) {
+      showSwal("کاربری یا این اطلاعات وجود دازد", "error", "تلاش بیشتر");
+    }
+  };
 
   return (
     <>
       {!isRegisterWithOtp ? (
         <>
           <div className={styles.form}>
-            <input className={styles.input} type="text" placeholder="نام" />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="نام"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
             <input
               className={styles.input}
               type="text"
               placeholder="شماره موبایل  "
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
             />
             <input
               className={styles.input}
               type="email"
               placeholder="ایمیل (دلخواه)"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             {isRegisterWithPassword && (
               <input
                 className={styles.input}
-                type="Otp"
+                type="password"
                 placeholder="رمز عبور"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             )}
             <p
@@ -39,7 +116,13 @@ const Register = ({ showloginForm }) => {
               ثبت نام با کد تایید
             </p>
             <button
-              onClick={() => setIsRegisterWithPassword(true)}
+              onClick={() => {
+                if (isRegisterWithPassword) {
+                  signUp();
+                } else {
+                  setIsRegisterWithPassword(true);
+                }
+              }}
               style={{ marginTop: ".7rem" }}
               className={styles.btn}
             >
